75c839493747bfbacfd30455c96159d0d02f78bb
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-06 14:17:26 +0100

    Saving data in database

--- src/UserData.js
+++ src/UserData.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { connect } from 'react-redux'
-import { inputChangeAction } from './state/userData';
+import { inputChangeAction, saveInDatabase } from './state/userData';
 
 const UserData = (props) => (
     <div>
@@ -9,6 +9,11 @@ const UserData = (props) => (
             value={props._userData}
             onChange={props._inputChangeAction}
         />
+        <button
+            onClick={props._saveToDatabase}
+        >
+            Add value
+        </button>
     </div>
 )
 
@@ -17,7 +22,8 @@ const mapStateToProps = state => ({
 })
 
 const dispatchToProps = dispatch => ({
-    _inputChangeAction: (event) => dispatch(inputChangeAction(event.target.value))
+    _inputChangeAction: (event) => dispatch(inputChangeAction(event.target.value)),
+    _saveToDatabase: () => dispatch(saveInDatabase())
 })
 
 
--- src/state/userData.js
+++ src/state/userData.js
@@ -1,26 +1,33 @@
+import { database } from '../firebaseConfig'
 
-const TEXT_VALUE = 'TEXT_VALUE'
+const TEXT_VALUE = 'userData/TEXT_VALUE'
 
 export const inputChangeAction = text => ({
     type: TEXT_VALUE,
     text
 })
 
+export const saveInDatabase = () => (dispatch, getState) => {
+    
+    const { userData: { text }} = getState()
+    
+    database.ref('user').push({
+        text
+    })
+}
+
 const INITIAL_STATE = {
     text: ''
 }
 
 export default (state = INITIAL_STATE, action) => {
-
     switch(action.type) {
-
         case TEXT_VALUE:
             return {
                 ...state,
                 text: action.text
             }
-
-
+        
         default:
             return state
     }
