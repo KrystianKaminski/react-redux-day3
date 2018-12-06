4e7df065ec6def60e7d479f274213be62f791628
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-06 10:10:53 +0100

    React-redux installed, Provider imported - all content wrapper

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(
-    <MuiThemeProvider>
-        <App />
-    </MuiThemeProvider>
+import { store } from './store'
 
-    , document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>
+    </Provider>,
+    document.getElementById('root'));
